b9ba17cc55a4c4175feeafe6e4df5fb6a83b0ab8
=ReptilianEye <=piotr.rzadkowski@outlook.com>
2023-06-08 10:46:32 +0200

    adding schemas for datatypes

---
+++ src/data/UserSchema.js
@@ -0,0 +1,12 @@
+
+all_roles = ["presenter", "photographer", "HTML","Python"];
+users = [
+    {
+        name: "Bob",
+        lastname: "Kowalski",
+        nickname: "Bob123",
+        roles: ["presenter", "HTML"],
+        project: [2, 3], //ids
+        todo:[] //
+    }
+]
---
+++ src/data/projectsSchema.js
@@ -0,0 +1,10 @@
+date = new Date();
+const project = [
+  {
+    name: "project 1",
+    author: "user 1",
+    description: "my first project",
+    dateCreated: date,
+    collaborators: [0,3], //ids
+  },
+];
